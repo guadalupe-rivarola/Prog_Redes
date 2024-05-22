795b26f2863e36467886a26a9e3f653e29ad7990
guadalupe-rivarola <guadalupe.rivarola.et32@gmail.com>
2024-05-22 12:41:57 -0300

    22/05

--- Archivos/src/main/java/Archivos/main.java
+++ Archivos/src/main/java/Archivos/main.java
@@ -5,7 +5,9 @@ public class main {
 	public static void main(String[] args) {
 		//arhivo de texto plano: tiene solo texto (sin formato
 		//volcado de memoria: escribir datos binario)
-
+		Ficheros archivo = new Ficheros();
+		archivo.createFilePrintStream(archivo.getArchivo()); 
+		archivo.createFilePrinter(archivo.getArchivo());
 	}
 
 }
