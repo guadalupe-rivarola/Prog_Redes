68887b868e984207c8006606c60c600a54cbc4ad
guadalupe-rivarola <guadalupe.rivarola.et32@gmail.com>
2024-06-15 17:08:59 -0300

    crear tp

---
+++ TP1/src/main/java/flujoDeDatos/Ejercicios.java
@@ -0,0 +1,24 @@
+package flujoDeDatos;
+
+import java.io.PrintStream;
+
+public class Ejercicios {
+
+
+		static public void Ejercicio1() {
+		
+		
+	}
+		
+		static public void Ejercicio2() {
+			
+			
+		}
+	
+		static public void Ejercicio3() {
+			
+			
+		}
+		
+		
+}
