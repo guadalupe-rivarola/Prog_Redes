1f096a19c2dbc270816a27c3add0e82f736d3d7c
guadalupe-rivarola <guadalupe.rivarola.et32@gmail.com>
2024-09-18 10:30:47 -0300

    arreglos

--- Hilos/src/main/java/Banco/Banco.java
+++ Hilos/src/main/java/Banco/Banco.java
@@ -7,27 +7,73 @@ import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
+/**
+ * Clase que representa un banco que gestiona cuentas y operaciones de transferencia.
+ * Utiliza mecanismos de bloqueo para asegurar la consistencia en las operaciones.
+ */
 public class Banco {
-	private Map<Integer,Integer> cuentas;
-	private PrintStream ps = new PrintStream(System.out);
-	private Lock cierreDeBanco = new ReentrantLock(); //permite bloquear 
-	private Condition saldoSuficiente; //se necesita una condicion para bloquear
-	
-	public Banco( int cantCuentas, int montoInicial) {
-		cuentas = new HashMap<>();
-		saldoSuficiente = cierreDeBanco.newCondition(); //vinculando un bloqueador con una condicion.
-		
-		
-		for(int nro=0; nro<cantCuentas;nro++) {
-			cuentas.put(nro, montoInicial);
-		}//for
-		
-	}//constructor
 
-	public void transferir(int cuentaOrigen, int cuentaDestino, int monto) {
-		// TODO Auto-generated method stub
-		
-		
-	}//transferir
+    private String nombreBanco;
+    private int sucursal;
+    private Map<Integer, Integer> cuentas;
+    private PrintStream ps = new PrintStream(System.out);
+    
+    /**
+     * Objeto Lock utilizado para manejar la sincronizaci�n en las operaciones del banco.
+     * Se utiliza para asegurar que solo un hilo pueda realizar operaciones cr�ticas a la vez.
+     */
+    private Lock cierreDeBanco = new ReentrantLock();
+    
+    /**
+     * Objeto Condition asociado al Lock {@code cierreDeBanco}.
+     * Se utiliza para esperar y notificar condiciones espec�ficas relacionadas con las operaciones de transferencia.
+     */
+    private Condition saldoSuficiente;
+    
+    /**
+     * Constructor para inicializar el objeto Banco.
+     * 
+     * @param cantCuentas N�mero de cuentas a crear en el banco.
+     * @param montoInicial Monto inicial de cada cuenta.
+     * @param nombre Nombre del banco.
+     */
+    public Banco(int cantCuentas, int montoInicial, String nombre) {
+        ps.printf("\n" + Colores.ANSI_WHITE_BACKGROUND + "\t\t\t" + Colores.ANSI_BLACK + "BANCO CREADO \t\t\t\n" + Colores.ANSI_RESET);
+
+        cuentas = new HashMap<>();
+        
+        // Inicializa el objeto Condition asociado al Lock cierreDeBanco.
+        // Esto permite que los hilos puedan esperar en una condici�n espec�fica y ser notificados cuando se cumpla.
+        saldoSuficiente = cierreDeBanco.newCondition();
+        
+        this.nombreBanco = nombre;
+        
+        // Inicializa las cuentas con el monto inicial proporcionado
+        for (int nro = 0; nro < cantCuentas; nro++) {
+            cuentas.put(nro, montoInicial);
+        }
+    }
+
+    /**
+     * Realiza una transferencia entre dos cuentas del banco.
+     * Este m�todo deber�a ser implementado para manejar la l�gica de transferencia
+     * y la sincronizaci�n necesaria para asegurar la consistencia de las cuentas.
+     * 
+     * @param cuentaOrigen N�mero de la cuenta de origen.
+     * @param cuentaDestino N�mero de la cuenta destino.
+     * @param monto Monto a transferir.
+     */
+    public void transferir(int cuentaOrigen, int cuentaDestino, int monto) {
+        // Implementar l�gica de transferencia aqu�
+    }
+
+    /**
+     * Obtiene el nombre del banco.
+     * 
+     * @return El nombre del banco.
+     */
+    public String getName() {
+        return this.nombreBanco;
+    }
 
 }
--- Hilos/src/main/java/Banco/Main.java
+++ Hilos/src/main/java/Banco/Main.java
@@ -1,23 +1,44 @@
 package Banco;
 
+import java.io.PrintStream;
 import java.util.Random;
 
-public class Main {
+/**
+ * Clase principal que inicializa el banco y crea hilos para realizar transferencias entre cuentas.
+ */
+public class main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Random rand = new Random(System.currentTimeMillis());
-		Banco nacion = new Banco(100 , 5000);
-		
-		for(int nroCuenta=0; nroCuenta<100 ;nroCuenta++) {
-			int monto ]= rand.nextInt(500, 5000);
-			Transferencias transfEnCurso = new Transferencias(nacion,nroCuenta,monto);
-			transfEnCurso.setName("Cuenta-" + nroCuenta);
-			
-			transfEnCurso.start();
-		}
-		
-
-	}
-
+    // Número total de cuentas a crear en el banco
+    public static int cantCuentas = 100;
+    
+    /**
+     * Método principal que ejecuta la aplicación.
+     * 
+     * @param args Argumentos de línea de comandos (no se utilizan en esta aplicación).
+     */
+    public static void main(String[] args) {
+        Long tiempoActual;
+        PrintStream ps = new PrintStream(System.out);
+        Random rand = new Random(System.currentTimeMillis());
+        
+        // Crea una instancia del banco con un número de cuentas y un monto inicial aleatorio
+        Banco bancoNacion = new Banco(cantCuentas, rand.nextInt(2000) + 3000, "Nacion");
+        
+        ps.printf(Utils.ANSI_WHITE_BACKGROUND + "\t\t\t" + Utils.ANSI_PURPLE + "C.C creadas: %d \t\t\n" + Utils.ANSI_RESET, cantCuentas);
+        ps.printf(Utils.ANSI_WHITE_BACKGROUND + "\t\t\t" + Utils.ANSI_PURPLE + "Banco %s \t\t\t" + Utils.ANSI_RESET, bancoNacion.getName());
+        
+        // Crea y arranca 100 hilos para realizar transferencias
+        for (int nroCuenta = 0; nroCuenta < 100; nroCuenta++) {
+            int monto = rand.nextInt(5000 - 500) + 500; // Monto entre 500 y 5000
+            tiempoActual = System.currentTimeMillis();
+            Transferencias transfEnCurso = new Transferencias(bancoNacion, nroCuenta, monto, tiempoActual);
+            
+            // Configura el nombre y la prioridad del hilo de transferencia
+            transfEnCurso.setName("Cuenta-" + nroCuenta);
+            transfEnCurso.setPriority(rand.nextInt(10) + 1); // Prioridad entre 1 y 10
+        
+            // Inicia el hilo de transferencia
+            transfEnCurso.start();
+        } // for
+    } // main
 }
