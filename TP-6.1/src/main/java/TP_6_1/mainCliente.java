bf91679992d58aff68126934fb2861987c9f397e
guadalupe-rivarola <guadalupe.rivarola.et32@gmail.com>
2024-11-20 09:22:18 -0300

    base del tp

---
+++ TP-6.1/src/main/java/TP_6_1/mainCliente.java
@@ -0,0 +1,11 @@
+package TP_6_1;
+
+public class mainCliente {
+
+	public static void main(String[] args) {
+		
+		Cliente cli = new Cliente();
+		
+	}
+
+}
---
+++ TP-6.1/src/main/java/TP_6_1/mainServidor.java
@@ -0,0 +1,11 @@
+package TP_6_1;
+
+public class mainServidor {
+
+	public static void main(String[] args) {
+		
+		Servidor server = new Servidor();
+		server.start();
+	}
+
+}
